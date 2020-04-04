ed79e9526c069dc46a65fbc7d8fe6c6a5d0ac7f6
guoguo <noobwu@126.com>
2020-04-04 15:13:19 +0800

    MySql
    MySql

---
+++ Scripts/MySql/DncRolePermissionMapping.sql
@@ -0,0 +1,13 @@
+INSERT INTO `DncRolePermissionMapping`(`RoleCode`, `PermissionCode`, `CreatedOn`) VALUES ('C7kPHIwu', 'I2U5lW40', '1900-01-20 15:22:12');
+INSERT INTO `DncRolePermissionMapping`(`RoleCode`, `PermissionCode`, `CreatedOn`) VALUES ('C7kPHIwu', 'N76RWAiv', '1900-01-20 15:22:12');
+INSERT INTO `DncRolePermissionMapping`(`RoleCode`, `PermissionCode`, `CreatedOn`) VALUES ('C7kPHIwu', 'Nry7I8go', '1900-01-20 15:22:12');
+INSERT INTO `DncRolePermissionMapping`(`RoleCode`, `PermissionCode`, `CreatedOn`) VALUES ('C7kPHIwu', 'tjJ40v0j', '1900-01-20 15:22:12');
+INSERT INTO `DncRolePermissionMapping`(`RoleCode`, `PermissionCode`, `CreatedOn`) VALUES ('C7kPHIwu', 'TS8060OV', '1900-01-20 15:22:12');
+INSERT INTO `DncRolePermissionMapping`(`RoleCode`, `PermissionCode`, `CreatedOn`) VALUES ('C7kPHIwu', 'x0Mo9SJX', '1900-01-20 15:22:12');
+INSERT INTO `DncRolePermissionMapping`(`RoleCode`, `PermissionCode`, `CreatedOn`) VALUES ('C7kPHIwu', 'y9o5fJVD', '1900-01-20 15:22:12');
+INSERT INTO `DncRolePermissionMapping`(`RoleCode`, `PermissionCode`, `CreatedOn`) VALUES ('WPbv96HC', '30K14RFe', '1900-01-20 21:14:42');
+INSERT INTO `DncRolePermissionMapping`(`RoleCode`, `PermissionCode`, `CreatedOn`) VALUES ('WPbv96HC', 'I2U5lW40', '1900-01-20 21:14:42');
+INSERT INTO `DncRolePermissionMapping`(`RoleCode`, `PermissionCode`, `CreatedOn`) VALUES ('WPbv96HC', 'PNfKDg06', '1900-01-20 21:14:42');
+INSERT INTO `DncRolePermissionMapping`(`RoleCode`, `PermissionCode`, `CreatedOn`) VALUES ('WPbv96HC', 'TS8060OV', '1900-01-20 21:14:42');
+INSERT INTO `DncRolePermissionMapping`(`RoleCode`, `PermissionCode`, `CreatedOn`) VALUES ('WPbv96HC', 'x0Mo9SJX', '1900-01-20 21:14:42');
+INSERT INTO `DncRolePermissionMapping`(`RoleCode`, `PermissionCode`, `CreatedOn`) VALUES ('WPbv96HC', 'y9o5fJVD', '1900-01-20 21:14:42');
---
+++ Scripts/MySql/DncUserRoleMapping.sql
@@ -0,0 +1,3 @@
+INSERT INTO `DncUserRoleMapping`(`UserGuid`, `RoleCode`, `CreatedOn`) VALUES ('20263DA4-AED4-4ABA-A6CC-B985C016858E', '9PeVLUMU', '1900-01-20 17:12:42');
+INSERT INTO `DncUserRoleMapping`(`UserGuid`, `RoleCode`, `CreatedOn`) VALUES ('684ECA5E-2AEB-4478-BA70-8A6B29F78696', 'WPbv96HC', '1900-01-20 17:10:40');
+INSERT INTO `DncUserRoleMapping`(`UserGuid`, `RoleCode`, `CreatedOn`) VALUES ('D7F32600-64C3-484D-A933-2D4A62BDA0BC', 'C7kPHIwu', '1900-01-20 14:36:25');
