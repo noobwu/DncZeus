a430bb17e7463428e12478a78e1986dea473e591
guoguo <noobwu@126.com>
2020-04-18 12:12:18 +0800

    D2Cms
    D2Cms

--- Noob.D2CMSApi/Entities/System/SysDictData.cs
+++ Noob.D2CMSApi/Entities/System/SysDictData.cs
@@ -26,82 +26,74 @@ namespace Noob.D2CMSApi.Entities
     public class SysDictData:Entity<int>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="SysDictData"/> class.
+        /// 字典数据
         /// </summary>
-        /// <param name="id">The identifier.</param>
-    	public SysDictData(int id) : base(id) {}
+        public SysDictData(int id) : base(id) { }
 
         /// <summary>
         /// 字典类型Id
         /// </summary>
-        /// <value>The dictionary code.</value>
-        public virtual int DictTypeId { get;set;}
+        public virtual int DictTypeId { get; set; }
         /// <summary>
         /// 字典排序
         /// </summary>
-        /// <value>The dictionary sort.</value>
-        public virtual int? DictSort { get;set;}
+        public virtual int DictSort { get; set; }
         /// <summary>
         /// 字典标签
         /// </summary>
-        /// <value>The dictionary label.</value>
-        public virtual string DictLabel { get;set;}
+        public virtual string DictLabel { get; set; }
         /// <summary>
-        /// 字典键值
+        /// 字典键值(字符串)
         /// </summary>
-        /// <value>The dictionary value.</value>
-        public virtual string DictValue { get;set;}
+        public virtual string DictValue { get; set; }
+        /// <summary>
+        /// 字典键值(数字)
+        /// </summary>
+        public virtual int DictNumber { get; set; }
         /// <summary>
         /// 字典类型
         /// </summary>
-        /// <value>The type of the dictionary.</value>
-        public virtual string DictType { get;set;}
+        public virtual string DictType { get; set; }
         /// <summary>
-        /// 样式属性（其他样式扩展）
+        /// 字典值类型
         /// </summary>
-        /// <value>The CSS class.</value>
-        public virtual string CssClass { get;set;}
+        public virtual byte DictValueType { get; set; }
+        /// <summary>
+        /// 字典值类型
+        /// </summary>
+        public virtual string CssClass { get; set; }
         /// <summary>
         /// 表格回显样式
         /// </summary>
-        /// <value>The list class.</value>
-        public virtual string ListClass { get;set;}
+        public virtual string ListClass { get; set; }
         /// <summary>
-        /// 是否默认（Y是 N否）
+        /// 是否默认(1:是,2:否）
         /// </summary>
-        /// <value>The is default.</value>
-        public virtual byte IsDefault { get;set;}
+        public virtual byte IsDefault { get; set; }
         /// <summary>
-        /// 状态（0正常 1停用）
+        /// 状态(1:正常,2:停用)
         /// </summary>
-        /// <value>The status.</value>
-        public virtual byte Status { get;set;}
+        public virtual byte Status { get; set; }
         /// <summary>
         /// 创建者
         /// </summary>
-        /// <value>The create by.</value>
-        public virtual string CreateBy { get;set;}
+        public virtual string CreateBy { get; set; }
         /// <summary>
         /// 创建时间
         /// </summary>
-        /// <value>The created at.</value>
-        public virtual int? CreatedAt { get;set;}
+        public virtual int? CreatedAt { get; set; }
         /// <summary>
         /// 更新者
         /// </summary>
-        /// <value>The update by.</value>
-        public virtual string UpdateBy { get;set;}
+        public virtual string UpdateBy { get; set; }
         /// <summary>
         /// 更新时间
         /// </summary>
-        /// <value>The updated at.</value>
-        public virtual int? UpdatedAt { get;set;}
+        public virtual int? UpdatedAt { get; set; }
         /// <summary>
         /// 备注
         /// </summary>
-        /// <value>The remark.</value>
-        public virtual string Remark { get;set;}
-
+        public virtual string Remark { get; set; }
 
 
         /// <summary>
--- Noob.D2CMSApi/Models/Models/System/DictDataModel.cs
+++ Noob.D2CMSApi/Models/Models/System/DictDataModel.cs
@@ -28,96 +28,90 @@ namespace Noob.D2CMSApi.Models
     public class DictDataModel : ModelBase
     {
         /// <summary>
-        /// Gets or sets the identifier.
+        /// 
         /// </summary>
-        /// <value>The identifier.</value>
         [JsonProperty("id")]
         public virtual int Id { get; set; }
         /// <summary>
         /// 字典类型Id
         /// </summary>
-        /// <value>The dictionary code.</value>
         [JsonProperty("dict_id")]
         public virtual int DictTypeId { get; set; }
         /// <summary>
         /// 字典排序
         /// </summary>
-        /// <value>The dictionary sort.</value>
         [JsonProperty("dict_sort")]
-        public virtual int? DictSort { get; set; }
+        public virtual int DictSort { get; set; }
         /// <summary>
         /// 字典标签
         /// </summary>
-        /// <value>The dictionary label.</value>
         [JsonProperty("dict_label")]
-        public virtual String DictLabel { get; set; }
+        public virtual string DictLabel { get; set; }
         /// <summary>
-        /// 字典键值
+        /// 字典键值(字符串)
         /// </summary>
-        /// <value>The dictionary value.</value>
         [JsonProperty("dict_value")]
-        public virtual String DictValue { get; set; }
+        public virtual string DictValue { get; set; }
+        /// <summary>
+        /// 字典键值(数字)
+        /// </summary>
+        [JsonProperty("dict_number")]
+        public virtual int DictNumber { get; set; }
         /// <summary>
         /// 字典类型
         /// </summary>
-        /// <value>The type of the dictionary.</value>
         [JsonProperty("dict_type")]
-        public virtual String DictType { get; set; }
+        public virtual string DictType { get; set; }
         /// <summary>
-        /// 样式属性（其他样式扩展）
+        /// 字典值类型
         /// </summary>
-        /// <value>The CSS class.</value>
+        [JsonProperty("dict_value_type")]
+        public virtual byte DictValueType { get; set; }
+        /// <summary>
+        /// 字典值类型
+        /// </summary>
         [JsonProperty("css_class")]
-        public virtual String CssClass { get; set; }
+        public virtual string CssClass { get; set; }
         /// <summary>
         /// 表格回显样式
         /// </summary>
-        /// <value>The list class.</value>
         [JsonProperty("list_class")]
-        public virtual String ListClass { get; set; }
+        public virtual string ListClass { get; set; }
         /// <summary>
-        /// 是否默认（Y是 N否）
+        /// 是否默认(1:是,2:否）
         /// </summary>
-        /// <value>The is default.</value>
         [JsonProperty("is_default")]
         public virtual byte IsDefault { get; set; }
         /// <summary>
-        /// 状态（0正常 1停用）
+        /// 状态(1:正常,2:停用)
         /// </summary>
-        /// <value>The status.</value>
         [JsonProperty("status")]
         public virtual byte Status { get; set; }
         /// <summary>
         /// 创建者
         /// </summary>
-        /// <value>The create by.</value>
         [JsonProperty("create_by")]
-        public virtual String CreateBy { get; set; }
+        public virtual string CreateBy { get; set; }
         /// <summary>
         /// 创建时间
         /// </summary>
-        /// <value>The created at.</value>
         [JsonProperty("created_at")]
         public virtual string CreatedAt { get; set; }
         /// <summary>
         /// 更新者
         /// </summary>
-        /// <value>The update by.</value>
         [JsonProperty("update_by")]
-        public virtual String UpdateBy { get; set; }
+        public virtual string UpdateBy { get; set; }
         /// <summary>
         /// 更新时间
         /// </summary>
-        /// <value>The updated at.</value>
         [JsonProperty("updated_at")]
         public virtual string UpdatedAt { get; set; }
         /// <summary>
         /// 备注
         /// </summary>
-        /// <value>The remark.</value>
         [JsonProperty("remark")]
-        public virtual String Remark { get; set; }
-
+        public virtual string Remark { get; set; }
 
     }
 }
