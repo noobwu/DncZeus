8fd480edc8ddea575067ae48b72c615eece54b54
guoguo <noobwu@126.com>
2020-04-18 17:53:11 +0800

    D2Cms
    D2Cms

---
+++ Noob.Core/Domain/Repositories/RepositoryBase.cs
@@ -0,0 +1,195 @@
+﻿// ***********************************************************************
+// Assembly         : Noob.Core
+// Author           : Administrator
+// Created          : 2020-04-18
+//
+// Last Modified By : Administrator
+// Last Modified On : 2020-04-18
+// ***********************************************************************
+// <copyright file="RepositoryBase.cs" company="Noob.Core">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using Noob.Domain.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Noob.Domain.Repositories
+{
+    /// <summary>
+    /// Class RepositoryBase.
+    /// Implements the <see cref="Noob.Domain.Repositories.BasicRepositoryBase{TEntity}" />
+    /// Implements the <see cref="Noob.Domain.Repositories.IRepository{TEntity}" />
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the t entity.</typeparam>
+    /// <seealso cref="Noob.Domain.Repositories.BasicRepositoryBase{TEntity}" />
+    /// <seealso cref="Noob.Domain.Repositories.IRepository{TEntity}" />
+    public abstract class RepositoryBase<TEntity> : BasicRepositoryBase<TEntity>, IRepository<TEntity>
+       where TEntity : class, IEntity
+    {
+
+        /// <summary>
+        /// Gets the type of the element(s) that are returned when the expression tree associated with this instance of <see cref="T:System.Linq.IQueryable" /> is executed.
+        /// </summary>
+        /// <value>The type of the element.</value>
+        public virtual Type ElementType => GetQueryable().ElementType;
+
+        /// <summary>
+        /// Gets the expression tree that is associated with the instance of <see cref="T:System.Linq.IQueryable" />.
+        /// </summary>
+        /// <value>The expression.</value>
+        public virtual Expression Expression => GetQueryable().Expression;
+
+        /// <summary>
+        /// Gets the query provider that is associated with this data source.
+        /// </summary>
+        /// <value>The provider.</value>
+        public virtual IQueryProvider Provider => GetQueryable().Provider;
+
+        /// <summary>
+        /// Withes the details.
+        /// </summary>
+        /// <returns>IQueryable&lt;TEntity&gt;.</returns>
+        public virtual IQueryable<TEntity> WithDetails()
+        {
+            return GetQueryable();
+        }
+
+        /// <summary>
+        /// Withes the details.
+        /// </summary>
+        /// <param name="propertySelectors">The property selectors.</param>
+        /// <returns>IQueryable&lt;TEntity&gt;.</returns>
+        public virtual IQueryable<TEntity> WithDetails(params Expression<Func<TEntity, object>>[] propertySelectors)
+        {
+            return GetQueryable();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the collection.</returns>
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return GetQueryable().GetEnumerator();
+        }
+
+        /// <summary>
+        /// Gets the queryable.
+        /// </summary>
+        /// <returns>IQueryable&lt;TEntity&gt;.</returns>
+        protected abstract IQueryable<TEntity> GetQueryable();
+
+        /// <summary>
+        /// Get a single entity by the given <paramref name="predicate" />.
+        /// It returns null if no entity with the given <paramref name="predicate" />.
+        /// It throws <see cref="T:System.InvalidOperationException" /> if there are multiple entities with the given <paramref name="predicate" />.
+        /// </summary>
+        /// <param name="predicate">A condition to find the entity</param>
+        /// <param name="includeDetails">Set true to include all children of this entity</param>
+        /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken" /> to observe while waiting for the task to complete.</param>
+        /// <returns>Task&lt;TEntity&gt;.</returns>
+        public abstract Task<TEntity> FindAsync(
+            Expression<Func<TEntity, bool>> predicate,
+            bool includeDetails = true,
+            CancellationToken cancellationToken = default);
+        /// <summary>
+        /// get as an asynchronous operation.
+        /// </summary>
+        /// <param name="predicate">A condition to filter entities</param>
+        /// <param name="includeDetails">Set true to include all children of this entity</param>
+        /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken" /> to observe while waiting for the task to complete.</param>
+        /// <returns>Task&lt;TEntity&gt;.</returns>
+        /// <exception cref="Noob.Domain.Entities.EntityNotFoundException"></exception>
+        public async Task<TEntity> GetAsync(
+         Expression<Func<TEntity, bool>> predicate,
+         bool includeDetails = true,
+         CancellationToken cancellationToken = default)
+        {
+            var entity = await FindAsync(predicate, includeDetails, cancellationToken);
+
+            if (entity == null)
+            {
+                throw new EntityNotFoundException(typeof(TEntity));
+            }
+
+            return entity;
+        }
+        /// <summary>
+        /// Deletes many entities by function.
+        /// Notice that: All entities fits to given predicate are retrieved and deleted.
+        /// This may cause major performance problems if there are too many entities with
+        /// given predicate.
+        /// </summary>
+        /// <param name="predicate">A condition to filter entities</param>
+        /// <param name="autoSave">Set true to automatically save changes to database.
+        /// This is useful for ORMs / database APIs those only save changes with an explicit method call, but you need to immediately save changes to the database.</param>
+        /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken" /> to observe while waiting for the task to complete.</param>
+        /// <returns>Task.</returns>
+        public abstract Task DeleteAsync(Expression<Func<TEntity, bool>> predicate, bool autoSave = false, CancellationToken cancellationToken = default);
+
+    }
+    /// <summary>
+    /// Class RepositoryBase.
+    /// Implements the <see cref="Noob.Domain.Repositories.RepositoryBase{TEntity}" />
+    /// Implements the <see cref="Noob.Domain.Repositories.IRepository{TEntity, TKey}" />
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the t entity.</typeparam>
+    /// <typeparam name="TKey">The type of the t key.</typeparam>
+    /// <seealso cref="Noob.Domain.Repositories.RepositoryBase{TEntity}" />
+    /// <seealso cref="Noob.Domain.Repositories.IRepository{TEntity, TKey}" />
+    public abstract class RepositoryBase<TEntity, TKey> : RepositoryBase<TEntity>, IRepository<TEntity, TKey>
+    where TEntity : class, IEntity<TKey>
+    {
+        /// <summary>
+        /// Gets the asynchronous.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="includeDetails">if set to <c>true</c> [include details].</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>Task&lt;TEntity&gt;.</returns>
+        public abstract Task<TEntity> GetAsync(TKey id, bool includeDetails = true, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Finds the asynchronous.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="includeDetails">if set to <c>true</c> [include details].</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>Task&lt;TEntity&gt;.</returns>
+        public abstract Task<TEntity> FindAsync(TKey id, bool includeDetails = true, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// delete as an asynchronous operation.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="autoSave">if set to <c>true</c> [automatic save].</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>Task.</returns>
+        public virtual async Task DeleteAsync(TKey id, bool autoSave = false, CancellationToken cancellationToken = default)
+        {
+            var entity = await FindAsync(id, cancellationToken: cancellationToken);
+            if (entity == null)
+            {
+                return;
+            }
+
+            await DeleteAsync(entity, autoSave, cancellationToken);
+        }
+    }
+}
