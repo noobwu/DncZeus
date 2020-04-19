e65ab154f05d448d47c71c67ceae4289d8790e18
guoguo <noobwu@126.com>
2020-04-19 17:18:31 +0800

    Autofac
    Autofac

---
+++ Noob.Core/Autofac/Extensions/DependencyInjection/AutofacHostBuilderExtensions.cs
@@ -0,0 +1,41 @@
+﻿// ***********************************************************************
+// Assembly         : Noob.Core
+// Author           : Administrator
+// Created          : 2020-04-19
+//
+// Last Modified By : Administrator
+// Last Modified On : 2020-04-19
+// ***********************************************************************
+// <copyright file="AutofacHostBuilderExtensions.cs" company="Noob.Core">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using Autofac;
+using Noob.Autofac;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Extensions.Hosting
+{
+    /// <summary>
+    /// Class AutofacHostBuilderExtensions.
+    /// </summary>
+    public static class AutofacHostBuilderExtensions
+    {
+        /// <summary>
+        /// Uses the autofac.
+        /// </summary>
+        /// <param name="hostBuilder">The host builder.</param>
+        /// <returns>IHostBuilder.</returns>
+        public static IHostBuilder UseAutofac(this IHostBuilder hostBuilder)
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            return hostBuilder.ConfigureServices((_, services) =>
+                {
+                    services.AddObjectAccessor(containerBuilder);
+                })
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory(containerBuilder));
+        }
+    }
+}
