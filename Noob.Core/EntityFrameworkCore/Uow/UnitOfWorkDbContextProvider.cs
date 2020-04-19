7038375e220c6d14723debe926ff171e3cba3b08
guoguo <noobwu@126.com>
2020-04-19 14:27:34 +0800

    Uow
    Uow

--- Noob.Core/EntityFrameworkCore/Uow/UnitOfWorkDbContextProvider.cs
+++ Noob.Core/EntityFrameworkCore/Uow/UnitOfWorkDbContextProvider.cs
@@ -11,14 +11,19 @@
 // </copyright>
 // <summary></summary>
 // ***********************************************************************
-using Noob.Data;
-using Noob.Uow.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Noob.Data;
+using Noob.EntityFrameworkCore;
+using Noob.EntityFrameworkCore.DependencyInjection;
 
-namespace Noob.EntityFrameworkCore.Uow
+namespace Noob.Uow.EntityFrameworkCore
 {
+    //TODO: Implement logic in DefaultDbContextResolver.Resolve in old ABP.
+
     /// <summary>
     /// Class UnitOfWorkDbContextProvider.
     /// Implements the <see cref="Noob.EntityFrameworkCore.IDbContextProvider{TDbContext}" />
@@ -26,31 +31,133 @@ namespace Noob.EntityFrameworkCore.Uow
     /// <typeparam name="TDbContext">The type of the t database context.</typeparam>
     /// <seealso cref="Noob.EntityFrameworkCore.IDbContextProvider{TDbContext}" />
     public class UnitOfWorkDbContextProvider<TDbContext> : IDbContextProvider<TDbContext>
-       where TDbContext : IEfCoreDbContext
+        where TDbContext : IEfCoreDbContext
     {
+        /// <summary>
+        /// The unit of work manager
+        /// </summary>
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
+        /// <summary>
+        /// The connection string resolver
+        /// </summary>
         private readonly IConnectionStringResolver _connectionStringResolver;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnitOfWorkDbContextProvider{TDbContext}"/> class.
+        /// </summary>
+        /// <param name="unitOfWorkManager">The unit of work manager.</param>
+        /// <param name="connectionStringResolver">The connection string resolver.</param>
         public UnitOfWorkDbContextProvider(
+            IUnitOfWorkManager unitOfWorkManager,
             IConnectionStringResolver connectionStringResolver)
         {
+            _unitOfWorkManager = unitOfWorkManager;
             _connectionStringResolver = connectionStringResolver;
         }
 
+        /// <summary>
+        /// Gets the database context.
+        /// </summary>
+        /// <returns>TDbContext.</returns>
+        /// <exception cref="Exception">A DbContext can only be created inside a unit of work!</exception>
         public TDbContext GetDbContext()
         {
-            //var connectionStringName = ConnectionStringNameAttribute.GetConnStringName<TDbContext>();
-            //var connectionString = _connectionStringResolver.Resolve(connectionStringName);
+            var unitOfWork = _unitOfWorkManager.Current;
+            if (unitOfWork == null)
+            {
+                throw new Exception("A DbContext can only be created inside a unit of work!");
+            }
 
-            //var dbContextKey = $"{typeof(TDbContext).FullName}_{connectionString}";
+            var connectionStringName = ConnectionStringNameAttribute.GetConnStringName<TDbContext>();
+            var connectionString = _connectionStringResolver.Resolve(connectionStringName);
 
-            //var databaseApi = unitOfWork.GetOrAddDatabaseApi(
-            //    dbContextKey,
-            //    () => new EfCoreDatabaseApi<TDbContext>(
-            //        CreateDbContext(unitOfWork, connectionStringName, connectionString)
-            //    ));
+            var dbContextKey = $"{typeof(TDbContext).FullName}_{connectionString}";
 
-            //return ((EfCoreDatabaseApi<TDbContext>)databaseApi).DbContext;
-            return default(TDbContext);
+            var databaseApi = unitOfWork.GetOrAddDatabaseApi(
+                dbContextKey,
+                () => new EfCoreDatabaseApi<TDbContext>(
+                    CreateDbContext(unitOfWork, connectionStringName, connectionString)
+                ));
+
+            return ((EfCoreDatabaseApi<TDbContext>)databaseApi).DbContext;
+        }
+
+        /// <summary>
+        /// Creates the database context.
+        /// </summary>
+        /// <param name="unitOfWork">The unit of work.</param>
+        /// <param name="connectionStringName">Name of the connection string.</param>
+        /// <param name="connectionString">The connection string.</param>
+        /// <returns>TDbContext.</returns>
+        private TDbContext CreateDbContext(IUnitOfWork unitOfWork, string connectionStringName, string connectionString)
+        {
+            var creationContext = new DbContextCreationContext(connectionStringName, connectionString);
+            using (DbContextCreationContext.Use(creationContext))
+            {
+                var dbContext = CreateDbContext(unitOfWork);
+                return dbContext;
+            }
+        }
+
+        /// <summary>
+        /// Creates the database context.
+        /// </summary>
+        /// <param name="unitOfWork">The unit of work.</param>
+        /// <returns>TDbContext.</returns>
+        private TDbContext CreateDbContext(IUnitOfWork unitOfWork)
+        {
+            return unitOfWork.Options.IsTransactional
+                ? CreateDbContextWithTransaction(unitOfWork)
+                : unitOfWork.ServiceProvider.GetRequiredService<TDbContext>();
+        }
+
+        /// <summary>
+        /// Creates the database context with transaction.
+        /// </summary>
+        /// <param name="unitOfWork">The unit of work.</param>
+        /// <returns>TDbContext.</returns>
+        public TDbContext CreateDbContextWithTransaction(IUnitOfWork unitOfWork)
+        {
+            var transactionApiKey = $"EntityFrameworkCore_{DbContextCreationContext.Current.ConnectionString}";
+            var activeTransaction = unitOfWork.FindTransactionApi(transactionApiKey) as EfCoreTransactionApi;
+
+            if (activeTransaction == null)
+            {
+                var dbContext = unitOfWork.ServiceProvider.GetRequiredService<TDbContext>();
+
+                var dbtransaction = unitOfWork.Options.IsolationLevel.HasValue
+                    ? dbContext.Database.BeginTransaction(unitOfWork.Options.IsolationLevel.Value)
+                    : dbContext.Database.BeginTransaction();
+
+                unitOfWork.AddTransactionApi(
+                    transactionApiKey,
+                    new EfCoreTransactionApi(
+                        dbtransaction,
+                        dbContext
+                    )
+                );
+
+                return dbContext;
+            }
+            else
+            {
+                DbContextCreationContext.Current.ExistingConnection = activeTransaction.DbContextTransaction.GetDbTransaction().Connection;
+
+                var dbContext = unitOfWork.ServiceProvider.GetRequiredService<TDbContext>();
+
+                if (dbContext.As<DbContext>().HasRelationalTransactionManager())
+                {
+                    dbContext.Database.UseTransaction(activeTransaction.DbContextTransaction.GetDbTransaction());
+                }
+                else
+                {
+                    dbContext.Database.BeginTransaction(); //TODO: Why not using the new created transaction?
+                }
+
+                activeTransaction.AttendedDbContexts.Add(dbContext);
+
+                return dbContext;
+            }
         }
     }
 }
