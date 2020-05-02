8b1be7b4b3da81fc730a6310d8c1f527195023b3
guoguo <noobwu@126.com>
2020-05-02 16:22:34 +0800

    EntityFrameworkCore
    EntityFrameworkCore

---
+++ Noob.Core/EntityFrameworkCore/EntityFrameworkCore/MySQL/EfCoreDbContextConfigurationContextMySQLExtensions.cs
@@ -0,0 +1,43 @@
+﻿// ***********************************************************************
+// Assembly         : Noob.Core
+// Author           : Administrator
+// Created          : 2020-05-02
+//
+// Last Modified By : Administrator
+// Last Modified On : 2020-05-02
+// ***********************************************************************
+// <copyright file="EfCoreDbContextConfigurationContextMySQLExtensions.cs" company="Noob.Core">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Noob.EntityFrameworkCore.DependencyInjection;
+namespace Noob.EntityFrameworkCore
+{
+    /// <summary>
+    /// Class EfCoreDbContextConfigurationContextMySQLExtensions.
+    /// </summary>
+    public static class EfCoreDbContextConfigurationContextMySQLExtensions
+    {
+        /// <summary>
+        /// Uses my SQL.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns>DbContextOptionsBuilder.</returns>
+        public static DbContextOptionsBuilder UseMySQL(
+           [NotNull] this EfCoreDbContextConfigurationContext context)
+        {
+            if (context.ExistingConnection != null)
+            {
+                return context.DbContextOptions.UseMySQL(context.ExistingConnection);
+            }
+            else
+            {
+                return context.DbContextOptions.UseMySQL(context.ConnectionString);
+            }
+        }
+    }
+}
---
+++ Noob.Core/EntityFrameworkCore/EntityFrameworkCore/MySQL/EfCoreDbContextOptionsMySQLExtensions.cs
@@ -0,0 +1,54 @@
+﻿// ***********************************************************************
+// Assembly         : Noob.Core
+// Author           : Administrator
+// Created          : 2020-05-02
+//
+// Last Modified By : Administrator
+// Last Modified On : 2020-05-02
+// ***********************************************************************
+// <copyright file="EfCoreDbContextOptionsMySQLExtensions.cs" company="Noob.Core">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using JetBrains.Annotations;
+using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Noob.EntityFrameworkCore
+{
+    /// <summary>
+    /// Class EfCoreDbContextOptionsMySQLExtensions.
+    /// </summary>
+    public static class EfCoreDbContextOptionsMySQLExtensions
+    {
+        /// <summary>
+        /// Uses my SQL.
+        /// </summary>
+        /// <param name="options">The options.</param>
+        /// <param name="mySQLOptionsAction">My SQL options action.</param>
+        public static void UseMySQL([NotNull] this EfCoreDbContextOptions options)
+        {
+            options.Configure(context =>
+            {
+                context.UseMySQL();
+            });
+        }
+
+        /// <summary>
+        /// Uses my SQL.
+        /// </summary>
+        /// <typeparam name="TDbContext">The type of the t database context.</typeparam>
+        /// <param name="options">The options.</param>
+        /// <param name="optionsAction">My SQL options action.</param>
+        public static void UseMySQL<TDbContext>([NotNull] this EfCoreDbContextOptions options)
+            where TDbContext : EfCoreDbContext<TDbContext>
+        {
+            options.Configure<TDbContext>(context =>
+            {
+                context.UseMySQL();
+            });
+        }
+    }
+}
